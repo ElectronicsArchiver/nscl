b6cbea0ee4caf0de8d41ebd517a3621127acc3e7
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-03-26 20:43:44 -0400

    Refactored Tests & Added Basic Styling

--- test/Test.js
+++ test/Test.js
@@ -18,52 +18,148 @@
  * this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-var Test = (() => {
-  'use strict';
-  return {
-    passed: 0,
-    failed: 0,
-    async include(tests) {
-      for(let test of tests) {
-        let src = test;
-        if (!src.startsWith("/")) {
-          src = `/test/${src}`;
-        }
-        if (!src.endsWith("_test.js")) {
-          src = `${src}_test.js`;
-        }
-        log(`Testing ${test}`);
-        this.passed = this.failed = 0;
-        try {
-          await include(src);
-        } catch (e) {
-          // we might omit some tests in publicly available code for Security
-          // reasons, e.g. embargoing new XSS vectors
-          log("Skipping test ", test, e);
-          continue;
-        }
-      }
-    },
-    async run(test, msg = "", callback = null) {
-      let r = false;
-      try {
-        r = await test();
-      } catch(e) {
-        error(e);
-      }
-      this[r ? "passed" : "failed"]++;
-      log(`[TEST] ${r ? "PASSED" : "FAILED"} ${msg || test}`);
-      if (typeof callback === "function") try {
-        await callback(r, test, msg);
-      } catch(e) {
-        error(e, "[TEST]");
-      }
-      return r;
-    },
-    report() {
-      let {passed, failed} = this;
-      log(`[TESTS] FAILED: ${failed}, PASSED: ${passed}, TOTAL ${passed + failed}.`);
-    }
-  };
 
-})();
+{
+	'use strict'
+	
+	const toStyle = (...attributes) =>
+		attributes.join(';');
+	
+	
+	const style_default = toStyle(
+		'color : inherit' ,
+	);
+	
+	const style_none = toStyle(
+		'color : #ce8ce9' ,
+		'font-weight : bold' ,
+		'border-radius : 2px' ,
+	);
+	
+	const style_code = toStyle(
+		'color : #fff' ,
+		'background-color : #343333' ,
+		'border-radius : 2px'
+	);
+	
+	const style_passed = toStyle(
+		'color : green' ,
+		'font-weight : bold' ,
+		'border-radius : 2px'
+	);
+	
+	const style_failed = toStyle(
+		'color : red' ,
+		'font-weight : bold' ,
+		'border-radius : 2px'
+	);
+	
+	
+	const { log } = console;
+	
+	let
+		passed = 0 ,
+		failed = 0 ;
+	
+	
+	function resolvePath(path){
+		
+		if(!path.startsWith('/'))
+			path = `/test/${ path }`;
+		
+		if(!path.endsWith('_test.js'))
+			path += '_test.js';
+			
+		return path;
+	}
+	
+	async function includeTest(tests = []){
+		
+		for(let test of tests){
+			
+			log(`Testing ${ test }`);
+			
+			passed = failed = 0;
+
+			const path = resolvePath(test);
+			
+			try { await include(path); }
+			catch ( error ) {
+				
+				/*
+				 *	We might omit some tests in publicly 
+				 *	available code for Security reasons, 
+				 *	e.g. embargoing new XSS vectors.
+				 */
+
+				log('Skipping Test',test,error);
+			}
+		}
+	}
+	
+	
+	async function run(test,message = test,callback){
+		
+		let success = false;
+		
+		try { success = await test(); }
+		catch(e) { error(e); }
+		
+		if(success){
+			passed++;
+			log(`%cPASSED  %c${ test }`,style_passed,style_code);
+		} else {
+			log(`%cFAILED  %c${ test }`,style_failed,style_code);
+		}
+		
+		if(callback)
+			try { await callback(success,test,message); }
+			catch (e) { error(e,'[TEST CALLBACK]'); }
+		
+		return success;
+	}
+	
+	
+	function report(){
+		
+		const count = passed + failed;
+		
+		if(count == 0)
+			return noneConducted();
+		
+		if(failed == 0)
+			return failedAll(count);
+		
+		if(passed == 0)
+			return passedAll(count);
+		
+		failedSome(failed,count);
+	}
+	
+	
+	function noneConducted(){
+		log(`%c\nNo Tests Have Been Conducted\n`,
+			style_none);
+	}
+	
+	function failedAll(count){
+		log(`\nAll %c${ count } %cTests Have %cPassed\n`,
+			style_passed,style_default,style_passed);
+	}
+	
+	function passedAll(count){
+		log(`\nAll %c${ count } %cTests Have %cFailed\n`,
+			style_failed,style_default,style_failed);
+	}
+	
+	function failedSome(failed,count){
+		log(`%\nc${ failed } %c/ ${ count } Tests Have %cFailed\n`,
+			style_failed,style_default,style_failed);
+	}
+	
+	
+	self.Test = {
+		include : includeTest ,
+		report , run 
+	};
+}
