484a9f2ac6fdad26fe58dd30ca43dadd8fb52f08
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-03-28 09:09:33 -0400

    Refactored TLD Update Script

--- TLD/update.js
+++ TLD/update.js
@@ -18,77 +18,135 @@
  * this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-"use strict";
 
-var fs = require('fs');
-var https = require('https');
-var punycode = require('punycode');
+'use strict';
 
-const args = process.argv.slice(2);
-const TLD_CACHE = "public_suffix_list.dat";
-const TLD_URL = `https://publicsuffix.org/list/${TLD_CACHE}`;
-const TLD_OUT = args[0] || "../common/tld.js";
 
-let ts = Date.now();
+const 
+	fileSystem = require('fs'),
+	punycode = require('punycode'),
+	https = require('https');
+ 
+const { argv } = process;
+const { log } = console;
 
-https.get(TLD_URL, res => {
-  res.pipe(fs.createWriteStream(TLD_CACHE));
-  res.on("end", function() {
-    console.log(`${TLD_URL} retrieved in ${Date.now() - ts}ms.`);
-    parse();
-  });
+const args = argv.slice(2);
+
+const
+	Cache = 'public_suffix_list.dat' ,
+	Url = `https://publicsuffix.org/list/${ Cache }` ,
+	Out = args[0] ?? '../common/tld.js' ;
+	
+
+const start = Date.now();
+
+
+https.get(Url,(response) => {
+	response.pipe(fileSystem.createWriteStream(Cache));
+	response.on('end',() => {
+		
+		const delta = Date.now(); - start;
+		
+		log(`${ Url } retrieved in ${ delta }ms.`);
+		parse();
+	});
 });
 
-function parse() {
-  let section;
 
-  const sections = /^\/\/\s*===BEGIN (ICANN|PRIVATE) DOMAINS===\s*$/;
-  const comment  = /^\/\/.*?/;
-  const splitter = /(\!|\*\.)?(.+)/;
 
-  const tlds = {};
+const
+	regex_sections = /^\/\/\s*===BEGIN (ICANN|PRIVATE) DOMAINS===\s*$/ ,
+	regex_splitter =  /(\!|\*\.)?(.+)/ ,
+	regex_comment = /^\/\/.*?/ ;
 
-  for(let line of fs.readFileSync(TLD_CACHE, 'utf8').split(/[\r\n]/)) {
-    line = line.trim();
+const { stringify } = JSON;
 
-    if(sections.test(line)) {
-      section = sections.exec(line)[1].toLowerCase();
-      tlds[section] = {};
-      continue;
-    }
+function parse(){
+	
+	let section;
+	
+	const tlds = {};
+	
+	
+	const lines = fileSystem
+		.readFileSync(Cache,'utf8')
+		.split(/[\r\n]/)
+		.map((line) => line.trim());
+	
+	
+	for(const line of lines){
+		
+		if(regex_sections.test(line)){
+			
+			section = regex_sections
+				.exec(line)[1]
+				.toLowerCase();
+				
+			tlds[section] = {};
+				
+			continue;
+		}
+		
+		if(!section)
+		 	continue;
+			
+		if(regex_comment.test(line))
+			continue;
+			
+		if(!regex_splitter.test(line))
+  			continue;
+			
+		const [ _ , modifier , tld ] = regex_splitter.exec(line);
+		const ascii = punycode.toASCII(tld);
+		
+		let { level } = ascii.split('.');
+		
+		switch(modifier){
+		case '*.':
+			level++;
+			break;
+		case '!':
+			level--;
+			break;
+		}
+			
+		tlds[section][ascii] = level;
+	}
+	
+	
+	if(!tlds.icann)
+		throw `Error in TLD parser`;
+		
+	if(!tlds.private)
+		throw `Error in TLD parser`;
+	
+	fileSystem.unlinkSync(Cache);
+	
+	let out = fileSystem.readFileSync(Out,'utf8');
+	let exitCode = 1;
+	
+	const json = stringify(tlds);
 
-    if(!section || comment.test(line) || !splitter.test(line))
-      continue;
-
-    let parts = splitter.exec(line);
-    let tld  = punycode.toASCII(parts[2]),
-      level = tld.split(".").length,
-      modifier = parts[1];
-
-    if(modifier == "*.") level++;
-    if(modifier == "!") level--;
-
-    tlds[section][tld] = level;
-  }
-
-  if(!(tlds.icann && tlds.private))
-    throw `Error in TLD parser`;
-
-  fs.unlinkSync(TLD_CACHE);
-
-  let tldOut = fs.readFileSync(TLD_OUT, 'utf8');
-  let json = JSON.stringify(tlds);
-  let exitCode = 1;
-  if (!tldOut.includes(json)) {
-    tldOut = /^s*\{/.test(tldOut) ? json
-        : tldOut.replace(/(\btlds = )\{[^]*?\};/, `$1${json};`);
-    fs.writeFileSync(TLD_OUT, tldOut);
-    console.log(`${TLD_OUT} updated!`)
-    exitCode = 0;
-  } else {
-    console.log(`${TLD_OUT} was already up-to-date, nothing to do.`);
-  }
-  console.log(`TLDs update finished in ${Date.now() - ts}ms`)
-  process.exit(exitCode);
-};
-
+	if(out.includes(json)){
+		
+		out = json;
+		
+		if(!/^s*\{/.test(out))
+			out = out.replace(/(\btlds = )\{[^]*?\};/,`$1${ json };`);
+	    
+		fileSystem.writeFileSync(Out,out);
+	    
+		log(`${ Out } updated!`);
+	    
+		exitCode = 0;
+		
+	} else {
+		log(`${ Out } was already up-to-date.`);
+	}
+	
+	const delta = Date.now() - start;
+	
+	log(`TLDs update finished in ${ delta }ms`);
+	
+	process.exit(exitCode);
+}
